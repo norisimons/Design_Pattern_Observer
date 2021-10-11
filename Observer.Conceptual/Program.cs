33a9fa0ba8aa3b4c119a839619599636f82a36a7
norisimons <42768523+norisimons@users.noreply.github.com>
2021-10-11 21:24:36 +0300

    Add project files.

---
+++ Observer.Conceptual/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DesignPatternObserver
+{
+    interface IFollower
+    {
+        public void Update(string name, string message);
+    }
+    interface ICompany
+    {
+        public void Follow(IFollower followers);
+        public void Unfollow(IFollower followers);
+        public void NotifyFollower(string message);
+    }
+
+    class Company : ICompany
+    {
+        private string _companyName;
+        private List<IFollower> _followers;
+
+        public Company(string userName)
+        {
+            _companyName = userName;
+            _followers = new List<IFollower>();
+        }
+
+        public void Follow(IFollower followers)
+        {
+            _followers.Add(followers);
+        }
+
+        public void Unfollow(IFollower followers)
+        {
+            _followers.Remove(followers);
+        }
+
+        public void NotifyFollower(string put)
+        {
+            foreach (var follower in _followers)
+            {
+                follower.Update(_companyName, put);
+            }
+        }
+
+        public void Post(string post)
+        {
+            Console.WriteLine(_companyName + " must bring " + post);
+            NotifyFollower(post);
+        }
+    }
+
+    class Follower : IFollower
+    {
+        private string _followerName;
+
+        public Follower(string followerName)
+        {
+            _followerName = followerName;
+        }
+        public void Update(string companyName, string put)
+        {
+            Console.WriteLine(_followerName + ", you have to see the " + put + " by " + companyName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diesel = new Company("Diesel");
+            var salomon = new Company("Salomon");
+            var leon = new Follower("Leon");
+            var monta = new Follower("Monta");
+            var joy = new Follower("Joy");
+            var carl = new Follower("Carl");
+
+            diesel.Follow(leon);
+            diesel.Follow(monta);
+            salomon.Follow(carl);
+            diesel.Post("new Jeans");
+            salomon.Post("new Shoes");
+            salomon.Unfollow(joy);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# Design Patterns in C#
+
+This repository is part of the (https://refactoring.guru/design-patterns) project.
+
+It contains C# examples for all classic GoF design patterns. Each pattern includes two examples:
+
+ **Conceptual** examples show the internal structure of patterns with detailed comments.
+ **RealWold** examples show how the patterns can be used in a real-world C# application.
+
+
+
+
+Most examples are console apps built using .NET Core 2.0.
+
+For the best experience, we recommend working with examples with these IDEs:
+
+- [Visual Studio 2017 and newer](https://www.visualstudio.com/downloads/) on Windows/Mac.
+- [Visual Studio Code](https://code.visualstudio.com/) on any OS.
+- [Rider](https://www.jetbrains.com/rider/) on any OS.
+
+
+
